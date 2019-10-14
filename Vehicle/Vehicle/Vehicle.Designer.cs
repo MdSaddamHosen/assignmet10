096848ff64d37a095aff57d682c78e735cc07149
DESKTOP-Q5FQUO6 <mdsaddam704804@gmail.com>
2019-10-14 22:55:26 +0600

    some file here

---
+++ Vehicle/Vehicle/Vehicle.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Vehicle
+{
+    partial class Vehicle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.vehicleNameTextBox = new System.Windows.Forms.TextBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.regNoTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.speedTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.minSpeedTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.maxSpeedTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.averageSpeedTextBox = new System.Windows.Forms.TextBox();
+            this.createButton = new System.Windows.Forms.Button();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vehicle Name";
+            // 
+            // vehicleNameTextBox
+            // 
+            this.vehicleNameTextBox.Location = new System.Drawing.Point(137, 43);
+            this.vehicleNameTextBox.Name = "vehicleNameTextBox";
+            this.vehicleNameTextBox.Size = new System.Drawing.Size(185, 20);
+            this.vehicleNameTextBox.TabIndex = 0;
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(289, 249);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(88, 29);
+            this.showButton.TabIndex = 2;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Reg No";
+            // 
+            // regNoTextBox
+            // 
+            this.regNoTextBox.Location = new System.Drawing.Point(137, 79);
+            this.regNoTextBox.Name = "regNoTextBox";
+            this.regNoTextBox.Size = new System.Drawing.Size(185, 20);
+            this.regNoTextBox.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(111, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Speed";
+            // 
+            // speedTextBox
+            // 
+            this.speedTextBox.Location = new System.Drawing.Point(193, 136);
+            this.speedTextBox.Name = "speedTextBox";
+            this.speedTextBox.Size = new System.Drawing.Size(129, 20);
+            this.speedTextBox.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Min Speed";
+            // 
+            // minSpeedTextBox
+            // 
+            this.minSpeedTextBox.Location = new System.Drawing.Point(97, 205);
+            this.minSpeedTextBox.Name = "minSpeedTextBox";
+            this.minSpeedTextBox.Size = new System.Drawing.Size(165, 20);
+            this.minSpeedTextBox.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 261);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Max Speed";
+            // 
+            // maxSpeedTextBox
+            // 
+            this.maxSpeedTextBox.Location = new System.Drawing.Point(97, 254);
+            this.maxSpeedTextBox.Name = "maxSpeedTextBox";
+            this.maxSpeedTextBox.Size = new System.Drawing.Size(165, 20);
+            this.maxSpeedTextBox.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 290);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Average Speed";
+            // 
+            // averageSpeedTextBox
+            // 
+            this.averageSpeedTextBox.Location = new System.Drawing.Point(97, 287);
+            this.averageSpeedTextBox.Name = "averageSpeedTextBox";
+            this.averageSpeedTextBox.Size = new System.Drawing.Size(165, 20);
+            this.averageSpeedTextBox.TabIndex = 5;
+            // 
+            // createButton
+            // 
+            this.createButton.Location = new System.Drawing.Point(356, 79);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(86, 24);
+            this.createButton.TabIndex = 2;
+            this.createButton.Text = "Create";
+            this.createButton.UseVisualStyleBackColor = true;
+            this.createButton.Click += new System.EventHandler(this.createButton_Click);
+            // 
+            // enterButton
+            // 
+            this.enterButton.Location = new System.Drawing.Point(383, 136);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(72, 24);
+            this.enterButton.TabIndex = 2;
+            this.enterButton.Text = "Enter";
+            this.enterButton.UseVisualStyleBackColor = true;
+            this.enterButton.Click += new System.EventHandler(this.enterButton_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(337, 142);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Kilo/Hr";
+            // 
+            // Vehicle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 333);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.createButton);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.averageSpeedTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.maxSpeedTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.minSpeedTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.speedTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.regNoTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.vehicleNameTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Vehicle";
+            this.Text = "Vehicle";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox vehicleNameTextBox;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox regNoTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox speedTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox minSpeedTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox maxSpeedTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox averageSpeedTextBox;
+        private System.Windows.Forms.Button createButton;
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Vehicle/Vehicle/Vehicle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vehicle
+{
+    public partial class Vehicle : Form
+    {
+        VehicleAnother _vehicle;
+        public Vehicle()
+        {
+            InitializeComponent();
+        }
+
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            _vehicle = new VehicleAnother(vehicleNameTextBox.Text, regNoTextBox.Text);
+
+         vehicleNameTextBox.Clear();
+             regNoTextBox.Clear();
+
+        }
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            _vehicle.Speed = Convert.ToDouble(speedTextBox.Text);
+            _vehicle.SetSpeed();
+
+           speedTextBox.Clear();
+
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            minSpeedTextBox.Text = _vehicle.GetMinSpeed().ToString();
+            maxSpeedTextBox.Text = _vehicle.GetMaxSpeed().ToString();
+            averageSpeedTextBox.Text = _vehicle.GetAverageSpeed().ToString();
+
+        }
+    }
+}
